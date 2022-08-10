6ffb7fffde222b40fcd3787d3fc751c85224ebed
troy <troymnlsn@gmail.com>
2022-08-10 11:52:48 -0500

    initial

---
+++ model/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        logging: false
+    }
+);
+
+module.exports = connection;
